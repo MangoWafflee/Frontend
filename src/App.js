03fb28ac111363fc92bbc387b082c15a40c2a3f0
SanghyunLee <lgw9736@naver.com>
2024-07-15 19:33:34 +0900

    기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main/Main.jsx";
+import Friend from "./pages/Friend/Friend.jsx";
+import Challenge from "./pages/Challenge/Challenge.jsx";
+import Mypage from "./pages/Mypage/Mypage.jsx";
 
 function App() {
-  return (
-      <div className="page">
-        <div className="main_box">
-          <div className='wrap'> 
-            content
-          </div>
-          <Navbar/>
-        </div>
-      </div>
-  );
+	return (
+		<div className="page">
+			<div className="main_box">
+				<div className="wrap">
+					<Routes>
+						<Route path="/app" element={<Main />} />
+            <Route path="/friend" element={<Friend />} />
+            <Route path="/challenge" element={<Challenge />} />
+            <Route path="/mypage" element={<Mypage />} />
+					</Routes>
+				</div>
+				<Navbar />
+			</div>
+		</div>
+	);
 }
 export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
-import './Navbar.scss';
+import React from "react";
+import "./Navbar.scss";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <nav className='nav_container'>
-      <ul className='nav_bar'>
-        <li>홈</li>
-        <li>친구</li>
-        <li>챌린지</li>
-        <li>마이</li>
-      </ul>
-    </nav>
-  )
+	return (
+		<nav className="nav_container">
+			<ul className="nav_bar">
+				<li>
+					<NavLink to="/app"  className="nav-link" activeClassName="active">
+						홈
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/friend" className="nav-link" activeClassName="active">
+						친구
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/challenge" className="nav-link" activeClassName="active">
+						챌린지
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/mypage" className="nav-link" activeClassName="active">
+						마이
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 }
