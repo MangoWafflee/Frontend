1cce4f2b4e08433367c2bcb8c71b520c903f4625
NanaManBo <kimbosung22@naver.com>
2024-07-21 23:35:29 +0900

    🐛

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,11 +4,11 @@ import './Header.scss';
 export default function Header() {
   return (
     <header>
-        {/* 앱 로고 및 알림 아이콘 */}
-			<div className="top_layer">
-				<div className="logo">logo</div>
-				<div>notification</div>
-			</div>
+      {/* 앱 로고 및 알림 아이콘 */}
+      <div className="top_layer">
+        <div className="logo">logo</div>
+        <div>notification</div>
+      </div>
     </header>
-  )
+  );
 }
--- src/features/weekCalendar/weekCalendarSlice.js
+++ src/features/weekCalendar/weekCalendarSlice.js
@@ -1,27 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-
-const initialState={
-    // 일주일치 웃음과 관련된 데이터
-    value:[],
+const initialState = {
+  // 일주일치 웃음과 관련된 데이터
+  value: [],
 };
 
-export const weekCalendarSlice=createSlice({
-    name:'weekCalendar',
-    initialState,
-    reducers:{
-        // 일주일 전 달력 확인
-        previousWeek:(state)=>{
-
-        },
-        // 일주일 후 달력 확인
-        nextWeek:(state)=>{
-
-        },
-    }
-})
+export const weekCalendarSlice = createSlice({
+  name: 'weekCalendar',
+  initialState,
+  reducers: {
+    // 일주일 전 달력 확인
+    previousWeek: (state) => {},
+    // 일주일 후 달력 확인
+    nextWeek: (state) => {},
+  },
+});
 
 // 상태에 대한 선택자 정의
-export const selectValue=(state)=>state.weekCalendar.value;
+export const selectValue = (state) =>
+  state.weekCalendar.value;
 
 export default weekCalendarSlice.reducer;
