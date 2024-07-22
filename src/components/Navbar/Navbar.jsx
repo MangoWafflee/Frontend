118217348f857717d866c1fd61c70442bdd5ef6d
NanaManBo <kimbosung22@naver.com>
2024-07-22 23:48:20 +0900

    🐛

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Navbar.scss';
+import { NavLink } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className="nav_container">
+      <ul className="nav_bar">
+        <li>
+          <NavLink
+            to="/app"
+            className="nav-link"
+            activeClassName="active"
+          >
+            홈
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/friend"
+            className="nav-link"
+            activeClassName="active"
+          >
+            친구
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/challenge"
+            className="nav-link"
+            activeClassName="active"
+          >
+            챌린지
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            className="nav-link"
+            activeClassName="active"
+          >
+            프로필
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/camerarecognition"
+            className="nav-link"
+            activeClassName="active"
+          >
+            카메라
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
