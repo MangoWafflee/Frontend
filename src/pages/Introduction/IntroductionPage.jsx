1d78a06d4fd7cef9771f34c25ce56daca625d509
SanghyunLee <lgw9736@naver.com>
2024-07-30 15:50:53 +0900

    💄 소개 페이지 디자인

--- src/pages/Introduction/IntroductionPage.jsx
+++ src/pages/Introduction/IntroductionPage.jsx
@@ -1,47 +1,152 @@
-import React, { useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/pagination";
+import React, { useEffect, useState } from "react";
 import "./IntroductionPage.scss";
 import kakaoLoginImage from "../../assets/kakao_login_large_wide.png";
-import Header from "../../components/Header/Header";
 import useNotification from "../../hooks/useNotification";
 import { useNavigate, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function IntroductionPage() {
-	// 로그인 여부 확인 후 로그인 되어 있으면 메인페이지로 이동
-
 	const navigate = useNavigate();
 	const notify = useNotification();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+	// 메뉴버튼 클릭
+	const handleClick = () => {
+		setIsMenuOpen((prev) => !prev);
+	};
+
+	// 스크롤 맨 위로
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			// behavior: "smooth",
+		});
+	};
+
+	// 카카오 리다이렉트 URI, REST_API_KEY
+	const kakaoRedirectURI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
+	const kakaoRestApiKey = process.env.REACT_APP_REST_API_KEY;
+
+	const loginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoRestApiKey}&redirect_uri=${kakaoRedirectURI}&response_type=code`;
+
+	// 카카오 로그인 버튼 누를 시
+	const handleLogin = async () => {
+		window.location.href = loginUrl;
+		console.log(loginUrl);
+	};
+
+	// 로그인 여부 확인 후 로그인 되어 있으면 메인페이지로 이동
 	useEffect(() => {
 		if (false) navigate("/app");
 	}, [navigate]);
 
 	return (
 		<div className="introduction_page">
-			<span>앱 이름6 </span>
-			<Swiper
-				pagination={{
-					dynamicBullets: true,
-				}}
-				modules={[Pagination]}
-			>
-				<SwiperSlide>Slide 1</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
-				<SwiperSlide>Slide 4</SwiperSlide>
-				<SwiperSlide>Slide 5</SwiperSlide>
-				<SwiperSlide>Slide 6</SwiperSlide>
-			</Swiper>
-			<div className="kakao_login_button">
-				<a href="https://mango.angrak.cloud">
-					<img src={kakaoLoginImage} alt="카카오 로그인 버튼" />
-				</a>
-			</div>
+			{/* 메뉴바 */}
+			{isMenuOpen && (
+				<div className="menu">
+					<button>
+						<FontAwesomeIcon
+							icon={faX}
+							style={{ justifySelf: "flex-end" }}
+							onClick={handleClick}
+							size="xl"
+						/>
+					</button>
+					<ul>
+						<li
+							onClick={() => {
+								handleClick();
+								scrollToTop();
+							}}
+						>
+							<span>서비스 소개</span>
+						</li>
+						<li>
+							적을만한거 있나
+						</li>
+					</ul>
+				</div>
+			)}
+			{isMenuOpen && <div className="overlay"></div>}
+			{/* 상단바 */}
+			<header>
+				<span>망고와플</span>
+				<FontAwesomeIcon
+					icon={faBars}
+					style={{ cursor: "pointer" }}
+					onClick={handleClick}
+				/>
+			</header>
+			<div className="empty"></div>
+			{/* 소개 메인 */}
+			<section>
+				<div className="container"></div>
+				<h2>망고와플</h2>
+				<h2 style={{ color: "green" }}>웃음을 기록하다</h2>
+				<img src="r" alt="관련 이미지" />
+			</section>
+			{/* 앱 기능 소개 */}
+			<section id="app-functions">
+				<h2>앱의 주요 기능들</h2>
+				<ul>
+					<li className="function">
+						<div className="container">
+							<strong className="function-title">
+								친구 기능
+							</strong>
+							<p className="function-content">
+								웃음을 통해 친구와 선의의 경쟁을 해보아요
+							</p>
+							<img src="r" alt="관련 이미지" />
+						</div>
+					</li>
+					<li className="function">
+						<div className="container">
+							<strong className="function-title">
+								웃음 달력
+							</strong>
+							<p className="function-content">
+								웃음을 기록해주어요
+							</p>
+							<img src="r" alt="관련 이미지" />
+						</div>
+					</li>
+					<li className="function">
+						<div className="container">
+							<strong className="function-title">
+								기록 달성
+							</strong>
+							<p className="function-content">
+								기록을 달성해보아요
+							</p>
+							<img src="r" alt="관련 이미지" />
+						</div>
+					</li>
+					<li className="function">
+						<div className="container">
+							<strong className="function-title">챌린지</strong>
+							<p className="function-content">
+								다양한 사람들과 챌린지를 도전하며 웃어봐요
+							</p>
+							<img src="r" alt="관련 이미지" />
+						</div>
+					</li>
+				</ul>
+			</section>
 
-			<div className="to_main" onClick={() => navigate("/app")}>메인 페이지로!</div>
+			{/* 카카오 로그인 버튼 */}
+			<img
+				className="kakao_login_button"
+				src={kakaoLoginImage}
+				alt="카카오 로그인 버튼"
+				onClick={handleLogin}
+			/>
+
+			<div className="to_main" onClick={() => navigate("/app")}>
+				메인 페이지로!
+			</div>
 
 			<div
 				onClick={() => {
