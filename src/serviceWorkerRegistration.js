c8e05c32c9840943ff31fbbe1fdbe5826a9c49ed
SanghyunLee <lgw9736@naver.com>
2024-07-30 16:08:05 +0900

    🐛

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,107 +1,107 @@
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-  window.location.hostname === '[::1]' ||
-  window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$/
-  )
-);
+// const isLocalhost = Boolean(
+//   window.location.hostname === 'localhost' ||
+//   window.location.hostname === '[::1]' ||
+//   window.location.hostname.match(
+//     /^127(?:\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])){3}$/
+//   )
+// );
 
-export function register(config) {
-  // 브라우저가 서비스 워커를 지원하는지 확인
-  if ('serviceWorker' in navigator) {
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicUrl.origin !== window.location.origin) {
-      return;
-    }
+// export function register(config) {
+//   // 브라우저가 서비스 워커를 지원하는지 확인
+//   if ('serviceWorker' in navigator) {
+//     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+//     if (publicUrl.origin !== window.location.origin) {
+//       return;
+//     }
 
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+//     window.addEventListener('load', () => {
+//       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (isLocalhost) {
-        // 로컬 환경에서 서비스 워커가 유효한지 확인하는 함수 호출
-        // 실제 서비스 워커를 바로 등록하지 않고, 유효성을 검사하는 과정을 거침
-        // checkValidServiceWorker(swUrl, config);
+//       if (isLocalhost) {
+//         // 로컬 환경에서 서비스 워커가 유효한지 확인하는 함수 호출
+//         // 실제 서비스 워커를 바로 등록하지 않고, 유효성을 검사하는 과정을 거침
+//         // checkValidServiceWorker(swUrl, config);
 
-        // 임의 조치
-        registerValidSW(swUrl, config);
-      } else {
-        // 로컬 호스트가 아닌 경우 서비스 워커를 바로 등록
-        registerValidSW(swUrl, config);
-      }
-    });
-  }
-}
+//         // 임의 조치
+//         registerValidSW(swUrl, config);
+//       } else {
+//         // 로컬 호스트가 아닌 경우 서비스 워커를 바로 등록
+//         registerValidSW(swUrl, config);
+//       }
+//     });
+//   }
+// }
 
-function registerValidSW(swUrl, config) {
-  navigator.serviceWorker
-    .register(swUrl)
-    .then((registration) => {
-      registration.onupdatefound = () => {
-        const installingWorker = registration.installing;
-        if (installingWorker == null) {
-          return;
-        }
-        installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed') {
-            if (navigator.serviceWorker.controller) {
-              console.log(
-                'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See https://cra.link/PWA.'
-              );
+// function registerValidSW(swUrl, config) {
+//   navigator.serviceWorker
+//     .register(swUrl)
+//     .then((registration) => {
+//       registration.onupdatefound = () => {
+//         const installingWorker = registration.installing;
+//         if (installingWorker == null) {
+//           return;
+//         }
+//         installingWorker.onstatechange = () => {
+//           if (installingWorker.state === 'installed') {
+//             if (navigator.serviceWorker.controller) {
+//               console.log(
+//                 'New content is available and will be used when all ' +
+//                   'tabs for this page are closed. See https://cra.link/PWA.'
+//               );
 
-              if (config && config.onUpdate) {
-                config.onUpdate(registration);
-              }
-            } else {
-              console.log('Content is cached for offline use.');
+//               if (config && config.onUpdate) {
+//                 config.onUpdate(registration);
+//               }
+//             } else {
+//               console.log('Content is cached for offline use.');
 
-              if (config && config.onSuccess) {
-                config.onSuccess(registration);
-              }
-            }
-          }
-        };
-      };
-    })
-    .catch((error) => {
-      console.error('Error during service worker registration:', error);
-    });
-}
+//               if (config && config.onSuccess) {
+//                 config.onSuccess(registration);
+//               }
+//             }
+//           }
+//         };
+//       };
+//     })
+//     .catch((error) => {
+//       console.error('Error during service worker registration:', error);
+//     });
+// }
 
-function checkValidServiceWorker(swUrl, config) {
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
-  })
-    .then((response) => {
-      const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
-        navigator.serviceWorker.ready.then((registration) => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
-      } else {
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      );
-    });
-}
+// function checkValidServiceWorker(swUrl, config) {
+//   fetch(swUrl, {
+//     headers: { 'Service-Worker': 'script' },
+//   })
+//     .then((response) => {
+//       const contentType = response.headers.get('content-type');
+//       if (
+//         response.status === 404 ||
+//         (contentType != null && contentType.indexOf('javascript') === -1)
+//       ) {
+//         navigator.serviceWorker.ready.then((registration) => {
+//           registration.unregister().then(() => {
+//             window.location.reload();
+//           });
+//         });
+//       } else {
+//         registerValidSW(swUrl, config);
+//       }
+//     })
+//     .catch(() => {
+//       console.log(
+//         'No internet connection found. App is running in offline mode.'
+//       );
+//     });
+// }
 
-export function unregister() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready
-      .then((registration) => {
-        registration.unregister();
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  }
-}
+// export function unregister() {
+//   if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.ready
+//       .then((registration) => {
+//         registration.unregister();
+//       })
+//       .catch((error) => {
+//         console.error(error.message);
+//       });
+//   }
+// }
