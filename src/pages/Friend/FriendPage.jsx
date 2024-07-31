d591177110bda0487bc1515228cd310b99557823
jihukimme <jihu0210@naver.com>
2024-08-01 00:41:25 +0900

    🚧

--- src/pages/Friend/FriendPage.jsx
+++ src/pages/Friend/FriendPage.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import "./FriendPage.scss";
-import { Avatar, Box, Divider, Typography, Button } from "@mui/material";
+// import { ReactComponent as SearchSVG } from "../../assets/icons/Search.svg";
+// import { ReactComponent as AddFriendSVG } from "../../assets/icons/AddFriend.svg";
+
+import { Avatar, Box, Divider, Typography } from "@mui/material";
+import { useRef } from "react";
 import UserDefaultImage from "../../assets/images/UserDefaultImage.png";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import FriendItem from "./FriendItem";
-
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+import UserCard from "../../components/UserCard/UserCard";
 
 export default function FriendPage() {
   const [searchText, setSearchText] = useState(""); // 검색창 값
@@ -13,127 +15,77 @@ export default function FriendPage() {
   const [searchUserImage, setSearchUserImage] = useState(""); // 검색한 유저 이미지
   const [searchUserNickname, setSearchUserNickname] = useState(""); // 검색한 유저 닉네임
 
-  const [userId, setUserId] = useState(4); // 현재 로그인한 사용자의 ID
-  const [userDummyData, setUserDummyData] = useState([
+  const [isFriend, setIsFriend] = useState(false); // 친구 여부
+  const [isFriendApply, setIsFriendApply] = useState(false); // 친구 요청 수락 여부
+
+  //   const [friendList, setFriendList] = useState(null);
+  // 더미데이터
+  const friendList = [
     {
-      userId: 7,
       userName: "손흥민",
       userNickname: "son",
-      userImage: "UserDefaultImagDefaultImage",
-      isRequest: false,
-      isFollowing: false,
+      userImage: "https://cdn-icons-png.flaticon.com/512/4715/4715329.png",
     },
     {
-      userId: 23,
-      userName: "마마",
+      userName: "이상현",
       userNickname: "sang",
-      userImage: "UserDefaultImagDefaultImage",
-      isRequest: false,
-      isFollowing: false,
+      //   userImage: "https://cdn-icons-png.flaticon.com/512/4715/4715329.png",
     },
     {
-      userId: 13,
       userName: "박지성",
       userNickname: "ji",
-      userImage: "UserDefaultImagDefaultImage",
-      isRequest: false,
-      isFollowing: false,
+      userImage: "https://cdn-icons-png.flaticon.com/512/4715/4715329.png",
     },
-  ]);
+  ];
 
-  const handleButtonClick = async (user) => {
-    const { userId: receiverId, isRequest, isFollowing } = user;
-    if (!isRequest && !isFollowing) {
-      await handleFollowRequest(receiverId);
-    } else if (isRequest && !isFollowing) {
-      console.log("이미 팔로우 요청을 보냈습니다.");
-    } else if (isFollowing) {
-      // isFollowing == false 로 업데이트
-      await handleUnfollow(receiverId);
-    } else {
-      console.error("알 수 없는 상태입니다.");
-    }
-  };
+  //   useEffect(() => {
+  //   //API 요청(친구목록 가져오기)
+  //   let url;
 
-  // 팔로우 요청 함수
-  const handleFollowRequest = async (receiverId) => {
-    try {
-      const response = await fetch(`${BACKEND_URL}/follow/request`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          senderId: userId,
-          receiverId,
-        }),
-      });
+  // 	const fetchData = async () => {
+  // 		const response = await fetch(url, {
+  // 		  method: "GET",
+  // 		  headers: {
+  // 			"Content-Type": "application/json",
+  // 		  },
+  // 		});
 
-      if (!response.ok) {
-        const errorResponse = await response.text();
-        throw new Error(`팔로우 요청 실패: ${errorResponse}`);
-      }
+  // 		if (response.status === 404) {
+  // 			console.log(url);
+  // 		  console.log("검색 결과가 없습니다.");
+  // 		}
 
-      const updatedUserDummyData = userDummyData.map((user) => {
-        if (user.userId === receiverId) {
-          return { ...user, isRequest: true };
-        }
-        return user;
-      });
+  // 		if (response.status === 200) {
+  // 		  const data = await response.json(); // response.json()이 완료될 때까지 기다림
 
-      setUserDummyData(updatedUserDummyData);
+  //   		  console.log(url);
+  // 		  console.log(data);
 
-      console.log(`${receiverId}님께 팔로우를 요청합니다.`);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  // 언팔로우 함수
-  const handleUnfollow = async (receiverId) => {
-    try {
-      const response = await fetch(`${BACKEND_URL}/follow/unfollow`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          senderId: userId,
-          receiverId,
-        }),
-      });
-
-      if (!response.ok) {
-        const errorResponse = await response.text();
-        throw new Error(`언팔로우 실패: ${errorResponse}`);
-      }
-
-      // 언팔로우 상태 업데이트
-      const updatedUserDummyData = userDummyData.map((user) =>
-        user.userId === receiverId ? { ...user, isFollowing: false } : user
-      );
-      setUserDummyData(updatedUserDummyData);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+  // 		  setFriendList(data); // 상태 업데이트
+  // 		} else {
+  // 		  console.log("실패");
+  // 		}
+  // 	  };
+  // 	  fetchData();
+  // }, [friendList]);
 
   function searchFunction(e) {
     e.preventDefault();
     setSearchText(searchText);
     console.log("검색어:", searchText);
 
-    // 더미데이터를 찾는 로직
-    const searchResult = userDummyData.find(
-      (user) => user.userName === searchText || user.userNickname === searchText
+    // 더미데이터
+    setSearchUserName("손흥민");
+    setSearchUserImage(
+      "https://cdn-icons-png.flaticon.com/512/4715/4715329.png"
     );
-    if (searchResult) {
-      setSearchUserName(searchResult.userName);
-      setSearchUserImage(searchResult.userImage);
-      setSearchUserNickname(searchResult.userNickname);
-      setIsModalOpen(true); // 모달 열기
+    setSearchUserNickname("son");
+
+    //나중에 존재하는 닉네임인지 확인하는 걸로 바꿔야함
+    if (searchText === null || searchText === "") {
+      alert("검색어를 입력해주세요.");
     } else {
-      alert("검색 결과가 없습니다.");
+      setIsModalOpen(!isModalOpen); // 모달 열기/닫기 토글
     }
   }
 
@@ -153,7 +105,7 @@ export default function FriendPage() {
     }
   };
 
-  const handleFriendClick = (userId, userNickname, userName, userImage) => {
+  const handleFriendClick = (userNickname, userName, userImage) => {
     setSearchUserName(userName);
     if (userImage === undefined || userImage === null || userImage === "") {
       setSearchUserImage(UserDefaultImage);
@@ -161,67 +113,20 @@ export default function FriendPage() {
       setSearchUserImage(userImage);
     }
     setSearchUserNickname(userNickname);
+
     setIsModalOpen(!isModalOpen); // 모달 열기/닫기 토글
   };
-
-  // 친구 요청 목록 가져오기
-  const fetchFriendRequests = async () => {
-    try {
-      const response = await fetch(
-        `${BACKEND_URL}/follow/findRequestId/${userId}`
-      );
-
-      if (!response.ok) {
-        const errorText = await response.text();
-        if (errorText.includes("친구 추가 요청 내역을 찾을 수 없습니다.")) {
-          setUserDummyData([]);
-          // localStorage.setItem("userDummyData", JSON.stringify([]));
-          return;
-        }
-        throw new Error(`서버 응답 오류: ${errorText}`);
-      }
-
-      const serverData = await response.json();
-
-      const updatedUserDummyData = userDummyData.map((user) => {
-        const request = serverData.find(
-          (req) => req.receiver.id === user.userId
-        );
-        if (request) {
-          return { ...user, isRequest: request.status === "PENDING" };
-        }
-        return user;
-      });
-
-      setUserDummyData(updatedUserDummyData);
-      // localStorage.setItem(
-      //   "userDummyData",
-      //   JSON.stringify(updatedUserDummyData)
-      // );
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  useEffect(() => {
-    console.log(userDummyData);
-    fetchFriendRequests();
-  }, [userId]);
 
   return (
     <div className="friend-page">
       <SearchBar
         searchText={searchText}
         setSearchText={setSearchText}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("검색어:", searchText);
-          // 검색 결과 처리 로직 추가
-        }}
+        onSubmit={searchFunction}
       />
 
-      <div className="user-list">
-        <div className="user-list-bar">
+      <div className="friend-list">
+        <div className="friend-list-bar">
           <h3
             style={{
               fontWeight: "bold",
@@ -229,23 +134,67 @@ export default function FriendPage() {
               marginBottom: "0.2rem",
             }}
           >
-            유저
+            친구
           </h3>
           <Divider />
         </div>
-        {userDummyData.length > 0 ? (
-          userDummyData.map((user) => (
-            <FriendItem
-              key={user.userId}
-              friend={user}
-              onFollowRequest={() => handleFollowRequest(user.userId)}
-              onClick={() => {
-                handleButtonClick(user);
-              }}
-            />
-          ))
+        {friendList === null || friendList.length === 0 ? (
+          <div className="friend-list-empty">
+            <h3>친구를 추가해보세요!</h3>
+          </div>
         ) : (
-          <Typography>친구 목록이 비어있습니다.</Typography>
+          friendList.map((result, index) => (
+            <div
+              className="friend"
+              key={index}
+              onClick={() =>
+                handleFriendClick(
+                  result.userNickname,
+                  result.userName,
+                  result.userImage
+                )
+              }
+            >
+              <Box
+                sx={{ display: "flex", alignItems: "center", margin: "0.5rem" }}
+              >
+                <Avatar
+                  src={result.userImage}
+                  aria-label={result.userName}
+                  sx={{ width: 45, height: 45 }}
+                >
+                  <Typography sx={{ fontSize: "1rem" }}>
+                    {result.userName[0]}
+                  </Typography>
+                </Avatar>
+                <div className="friend-info">
+                  <Typography
+                    color="text.primary"
+                    sx={{
+                      fontSize: "1rem",
+                      textAlign: "left",
+                      marginLeft: "1rem",
+                      fontWeight: "bold",
+                    }}
+                    noWrap
+                  >
+                    {result.userName}
+                  </Typography>
+                  <Typography
+                    color="text.secondary"
+                    sx={{
+                      fontSize: "0.8rem",
+                      textAlign: "left",
+                      marginLeft: "1.5rem",
+                    }}
+                    noWrap
+                  >
+                    {result.userNickname}
+                  </Typography>
+                </div>
+              </Box>
+            </div>
+          ))
         )}
       </div>
 
@@ -253,13 +202,13 @@ export default function FriendPage() {
         <div className="modal-background" onClick={handleOutsideClick}>
           <div className="modal" ref={modalRef}>
             <div className="modal-content">
-              {/* <UserCard
+              <UserCard
                 userName={searchUserName}
                 userImage={searchUserImage}
                 userNickname={searchUserNickname}
                 isFriend={isFriend}
                 isFriendApply={isFriendApply}
-              ></UserCard> */}
+              ></UserCard>
             </div>
           </div>
         </div>
