58165ab00064f21dc63e3389c53d7920f268aeb9
SanghyunLee <lgw9736@naver.com>
2024-08-05 23:10:59 +0900

    🐛 개인기록 페이지 수정

--- src/pages/Achievement/AchievementPage.jsx
+++ src/pages/Achievement/AchievementPage.jsx
@@ -1,139 +1,118 @@
-import React, { useEffect, useState } from 'react';
-import { Divider } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import './AchievementPage.scss';
-import useFetchUserBadges from '../../hooks/useFetchUserBadges';
-import challenge_24_07_7 from '../../assets/challenges/challenge_24_07_7.png';
+import React, { useEffect, useState } from "react";
+import { Divider } from "antd";
+import { useNavigate } from "react-router-dom";
+import "./AchievementPage.scss";
+import useFetchUserBadges from "../../hooks/useFetchUserBadges";
+import axios from "../../app/axios";
+import { useQuery } from "@tanstack/react-query";
 
 export default function AchievementPage() {
-  const navigate = useNavigate();
-  const [uid, setUid] = useState('');
-  const [token, setToken] = useState('');
-  const { badgeList, error } = useFetchUserBadges(
-    uid,
-    token
-  );
+	const navigate = useNavigate();
+	const [uid, setUid] = useState("");
+	const [token, setToken] = useState("");
+	const [userId, setUserId] = useState(null);
+	const { badgeList, error } = useFetchUserBadges(uid, token);
 
-  useEffect(() => {
-    const storedUser = JSON.parse(
-      localStorage.getItem('user')
-    );
-    const storedToken = localStorage.getItem('token');
-    if (storedUser) {
-      setUid(storedUser.uid);
-      setToken(storedToken);
-    } else {
-      navigate('/');
-    }
-  }, [navigate]);
+	useEffect(() => {
+		const storedUser = JSON.parse(localStorage.getItem("user"));
+		const storedToken = localStorage.getItem("token");
+		if (storedUser) {
+			setUid(storedUser.uid);
+			setToken(storedToken);
+			setUserId(storedUser.id);
+		} else {
+			navigate("/");
+		}
+	}, [navigate]);
 
-  // 개인 기록
-  const individualRecordList = badgeList.map((badge) => ({
-    id: badge.id,
-    title: badge.title,
-    isAchieved: badge.isAchieved,
-    achievedAt: badge.achievedAt,
-    img: badge.image,
-  }));
+	// 개인 기록
+	const individualRecordList = badgeList.map((badge) => ({
+		id: badge.id,
+		title: badge.title,
+		isAchieved: badge.isAchieved,
+		achievedAt: badge.achievedAt,
+		img: badge.image,
+	}));
 
-  // 챌린지 기록
-  const challengeRecordList = [
-    {
-      id: 1,
-      title: '[7월] 7일 웃기',
-      isAchieved: true,
-      createdAt: '2024.07.12',
-      img: challenge_24_07_7,
-    },
-    {
-      id: 2,
-      title: '[7월] 7일 웃기',
-      isAchieved: true,
-      createdAt: '2024.07.12',
-      img: challenge_24_07_7,
-    },
-    {
-      id: 3,
-      title: '[7월] 7일 웃기',
-      isAchieved: true,
-      createdAt: '2024.07.12',
-      img: challenge_24_07_7,
-    },
-    {
-      id: 4,
-      title: '[7월] 7일 웃기',
-      isAchieved: true,
-      createdAt: '2024.07.12',
-      img: challenge_24_07_7,
-    },
-  ];
+	// 해당 유저의 참여했던 챌린지 정보 받아오는 api Query
+	const getUserChallenges = async () => {
+		const { data } = await axios.get(`/challenge/userchallenge/${userId}`);
+		const categorizedChallenges = {
+			// 참여했던 챌린지 데이터
+			participatedChallengeList: data.filter(
+				(challenge) =>
+					challenge.participating === "참여" &&
+					challenge.successStatus === "성공"
+			),
+		};
+		return categorizedChallenges;
+	};
 
-  return (
-    <div className="ahcievement-page">
-      <h2>개인 기록</h2>
-      <div className="badge-container">
-        {individualRecordList.length === 0 ||
-        individualRecordList === null ? (
-          <div>참여중인 챌린지가 없습니다.</div>
-        ) : (
-          individualRecordList.map((record) => (
-            <div
-              className="badge"
-              key={`individual-${record.id}`}
-            >
-              <img
-                src={record.img}
-                alt="challenge-image"
-                className={
-                  record.isAchieved === '성공'
-                    ? ''
-                    : 'grayscale'
-                }
-              />
-              <div className="column">
-                <p>{record.title}</p>
-                {record.isAchieved && (
-                  <p>{record.achievedAt}</p>
-                )}
-              </div>
-            </div>
-          ))
-        )}
-      </div>
+	// useQuery 훅으로 유저의 참여중인 챌린지/참여했던 챌린지 정보 가져오기
+	const {
+		data: userChallenges,
+		error: userChallengesError,
+		isLoading: isUserChallengesLoading,
+	} = useQuery({
+		queryKey: ["userChallenges", userId],
+		queryFn: getUserChallenges,
+		enabled: !!userId,
+	});
 
-      <Divider />
-      <h2>챌린지</h2>
-      <div className="badge-container">
-        {challengeRecordList.length === 0 ||
-        challengeRecordList === null ? (
-          <div className="challenge">
-            획득한 챌린지 뱃지가 없습니다.
-          </div>
-        ) : (
-          challengeRecordList.map((record) => (
-            <div
-              className="badge"
-              key={`challenge-${record.id}`}
-            >
-              <img
-                src={record.img}
-                alt="challenge-image"
-                className={
-                  record.isAchieved
-                    ? 'challenge-badge'
-                    : 'grayscale'
-                }
-              />
-              <div className="column">
-                <p>{record.title}</p>
-                {record.isAchieved && (
-                  <p>{record.createdAt}</p>
-                )}
-              </div>
-            </div>
-          ))
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="ahcievement-page">
+			<h2>개인 기록</h2>
+			<div className="badge-container">
+				{individualRecordList.length === 0 ||
+				individualRecordList === null ? (
+					<div>참여중인 챌린지가 없습니다.</div>
+				) : (
+					individualRecordList.map((record) => (
+						<div className="badge" key={`individual-${record.id}`}>
+							<img
+								src={record.img}
+								alt="challenge-image"
+								className={
+									record.isAchieved === "성공"
+										? ""
+										: "grayscale"
+								}
+							/>
+							<div className="column">
+								<p>{record.title}</p>
+								{record.isAchieved && (
+									<p>{record.achievedAt}</p>
+								)}
+							</div>
+						</div>
+					))
+				)}
+			</div>
+
+			<Divider />
+			<h2>챌린지</h2>
+			<div className="badge-container">
+				{isUserChallengesLoading ? (
+					<div>Loading...</div>
+				) : userChallengesError ? (
+					<div>Error: {userChallengesError.message}</div>
+				) : userChallenges?.participatingChallengeList?.length === 0 ? (
+					<div className="challenge">성공했던 챌린지가 없습니다.</div>
+				) : (
+					userChallenges?.participatingChallengeList?.map(
+						(challenge) => (
+							<div className="badge" key={challenge.id}>
+								<img
+									src={challenge.challenge.img}
+									alt="challenge-image"
+									className="challenge-badge"
+								/>
+								<span>{challenge.challenge.title}</span>
+							</div>
+						)
+					)
+				)}
+			</div>
+		</div>
+	);
 }
