701064ad4b33b0b12e98d8f19fadafa5012f13c5
NanaManBo <kimbosung22@naver.com>
2024-08-06 23:57:43 +0900

    농담 편집

--- src/components/RandomJoke/RandomJoke.jsx
+++ src/components/RandomJoke/RandomJoke.jsx
@@ -5,85 +5,52 @@ import './RandomJoke.scss';
 
 const jokes = [
   {
-    question: '왜 수학책은 항상 슬플까요?',
-    answer: '문제투성이니까요!',
+    question: '타이타닉의 구명보트는 몇 명이 탈수 있을까?',
+    answer: '9명',
   },
   {
-    question:
-      '벽에 걸린 시계가 수영을 배우려고 하면 뭐가 될까요?',
-    answer: '타이머!',
+    question: '한국에서 가장 급하게 만든 떡은 ?',
+    answer: '헐레벌떡',
   },
   {
-    question: '바나나가 자꾸 넘어지는 이유는 뭘까요?',
-    answer: '껍질을 벗고 있어서요!',
+    question: '개가 사람을 가르친다를 네 글자로 하면?',
+    answer: '개인지도',
   },
   {
-    question: '왜 달팽이는 항상 집을 가지고 다닐까요?',
-    answer: '집 없으면 고달프니까요!',
+    question: '거지가 빨간색 말을 타고 가는 것은?',
+    answer: '새빨간 거짓말',
   },
   {
-    question: '한 소가 다른 소에게 뭐라고 했을까요?',
-    answer: '"무~~~!"',
+    question: '새 중에서 가장 빠른 새는?',
+    answer: '눈 깜작할 새',
   },
   {
-    question: '왜 과학자는 책을 먹었을까요?',
-    answer: '지식을 소화하려고요!',
+    question: '도둑이 가장 싫어하는 아이스크림은?',
+    answer: '누가바',
   },
   {
-    question: '왜 컴퓨터는 바다를 싫어할까요?',
-    answer: '바이러스에 감염될까봐요!',
+    question: '미소의 반대말은?',
+    answer: '당기소!',
   },
   {
-    question: '커피는 왜 경찰을 좋아할까요?',
-    answer: '항상 잡아주니까요!',
+    question: '호주의 돈을 뭐라고 하는가?',
+    answer: '호주머니',
   },
   {
-    question: '왜 집이 웃을 수 있을까요?',
-    answer: '창문이 있어서요!',
+    question: '텔레토비가 차린 안경점 이름은?',
+    answer: '아이조아',
   },
   {
-    question: '왜 수박은 결혼할 수 없을까요?',
-    answer: '씨가 있어서요!',
+    question: '학생들이 싫어하는 피자는',
+    answer: '책 피자',
   },
   {
-    question: '왜 피아노는 항상 잠을 잘 자요?',
-    answer: '건반이 있어서요!',
+    question: '공이 웃으면?',
+    answer: '풋볼!',
   },
   {
-    question: '왜 바다에서는 수영을 할 수 없을까요?',
-    answer: '물이 다 짜서요!',
-  },
-  {
-    question: '왜 코끼리는 컴퓨터를 싫어할까요?',
-    answer: '너무 무겁게 느껴져서요!',
-  },
-  {
-    question: '왜 도넛은 경찰을 좋아할까요?',
-    answer: '항상 반갑게 맞아주니까요!',
-  },
-  {
-    question: '왜 숟가락은 항상 웃을까요?',
-    answer: '자꾸 떠서요!',
-  },
-  {
-    question: '왜 개구리는 항상 행복할까요?',
-    answer: '팔다리가 있어서요!',
-  },
-  {
-    question: '왜 비는 항상 노래를 부를까요?',
-    answer: '물방울들이 춤을 추니까요!',
-  },
-  {
-    question: '왜 초콜릿은 항상 달콤할까요?',
-    answer: '자꾸 녹아서요!',
-  },
-  {
-    question: '왜 거북이는 항상 느릴까요?',
-    answer: '집을 등에 메고 있어서요!',
-  },
-  {
-    question: '왜 유령은 수학을 좋아할까요?',
-    answer: '항상 소수점에 집착하니까요!',
+    question: '세상에서 제일 뜨거운 과일은?',
+    answer: '천도복숭아',
   },
 ];
 
